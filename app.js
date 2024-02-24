438e3e0b8e9c86b50237d5813307c618f64ceb19
Sanjana <sanjana.raj.25@dartmouth.edu>
2024-02-24 18:28:00 -0500

    initial text

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// add scroll animations using intersection observer
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=> {
         console.log(entry)
@@ -11,3 +12,19 @@ const observer = new IntersectionObserver((entries)=>{
 
 const hiddenElems = document.querySelectorAll('.hidden');
 hiddenElems.forEach((el) => observer.observe(el));
+
+
+// materialize css
+M.AutoInit();
+
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true,
+    duration: 100
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
